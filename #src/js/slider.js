f283c5dab6d4187c8dd168b33ba42221486f0dca
Vaone <gvin8888@gmail.com>
2021-07-14 14:37:43 +0500

    slider adaptive for slides

--- #src/js/slider.js
+++ #src/js/slider.js
@@ -34,20 +34,20 @@ const News = new Swiper(".news__swiper-slider", {
       disableOnInteraction: false,
     },
      */
-  slidesPerView: 4,
+  slidesPerView: "auto",
   spaceBetween: 15,
   // autoHeight: true,
   //  speed: 800,
   //  touchRatio: 0,
   //  simulateTouch: false,
   loop: true,
-  //  loopSlides: 3,
+  loopSlides: 3,
+  visibilityFullFit: true,
   //  preLoadImages: false,
   //  lazy: true,
   initialSlide: 1,
   centeredSlides: true,
-  // slidesOffsetBefore: -38,
-  // slidesOffsetAfter: 400,
+  
   // pagination
   pagination: {
     el: ".news__swiper-pagination",
@@ -68,60 +68,17 @@ const News = new Swiper(".news__swiper-slider", {
   // scrollbar: {
   // el: '.swiper-scrollbar',
   // }
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 0,
-      autoHeight: true,
-    },
-    500: {
-      slidesPerView: 1.5,
-      // spaceBetween: 10,
-    },
-    640: {
-      slidesPerView: 2,
-      // spaceBetween: 20,
-    },
-    810: {
-      slidesPerView: 2.5,
-      // spaceBetween: 20,
-    },
-    980: {
-      slidesPerView: 3,
-      spaceBetween: 20,
-    },
-    1150: {
-      slidesPerView: 3.5,
-      spaceBetween: 22,
-    },
-    1325: {
-      slidesPerView: 4,
-    },
-  },
 });
 
 const Experts = new Swiper(".experts__swiper-slider", {
-  /*
-    effect: 'fade',
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-     */
-  slidesPerView: 5,
-  spaceBetween: 22,
-  // autoHeight: true,
-  //  speed: 800,
-  //  touchRatio: 0,
-  //  simulateTouch: false,
+  slidesPerView: 'auto',
+  spaceBetween: 30,
   loop: true,
-  //  loopSlides: 3,
+  loopSlides: 3,
   //  preLoadImages: false,
   //  lazy: true,
   initialSlide: 1,
   centeredSlides: true,
-  // slidesOffsetBefore: 10,
-  // slidesOffsetAfter: 10,
   // pagination
   pagination: {
     el: ".experts__swiper-pagination",
@@ -142,43 +99,11 @@ const Experts = new Swiper(".experts__swiper-slider", {
   // scrollbar: {
   // el: '.swiper-scrollbar',
   // }
+  
   breakpoints: {
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 0,
-      autoHeight: true,
-    },
-    500: {
-      slidesPerView: 1.5,
-      // spaceBetween: 10,
-    },
-    640: {
-      slidesPerView: 2,
-      // spaceBetween: 20,
-    },
-    810: {
-      slidesPerView: 2.5,
-      // spaceBetween: 20,
-    },
-    980: {
-      slidesPerView: 3,
-      spaceBetween: 20,
-    },
-    1150: {
-      slidesPerView: 3.5,
+    1440: {
       spaceBetween: 22,
     },
-    1325: {
-      slidesPerView: 4,
-    },
-    1550: {
-      slidesPerView: 4.5,
-    },
-    1650: {
-      slidesPerView: 5,
-    },
-    // 1750: {
-    //   slidesPerView: 5.5,
-    // },
+    
   },
 });
