307b175d32ab39c9f1ee55c2da293556cf184726
Vaone <gvin8888@gmail.com>
2021-07-19 19:48:22 +0500

    settimeout

--- dist/js/app.js
+++ dist/js/app.js
@@ -5210,28 +5210,17 @@ if (menuLinks.length > 0) {
 
   function onMenuLinkClick(e) {
     const menuLink = e.target;
-    if (
-      menuLink.dataset.goto &&
-      document.querySelector(menuLink.dataset.goto)
-    ) {
+    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
       const gotoBlock = document.querySelector(menuLink.dataset.goto);
-      const gotoBlockValue =
-        gotoBlock.getBoundingClientRect().top +
-        pageYOffset
-        //  -
-        // document.querySelector("header").offsetHeight;
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset
 
-      // if (iconMenu.classList.contains("_active")) {
-      //   document.body.classList.remove("_lock");
-      //   iconMenu.classList.remove("_active");
-      //   menuBody.classList.remove("_active");
-      // }
-
-      window.scroll({
-        top: gotoBlockValue,
-        behavior: "smooth",
-      });
-      e.preventDefault();
+      setTimeout(function() {
+        window.scrollTo({
+          top: gotoBlockValue,
+          behavior: "smooth",
+        });
+        e.preventDefault();
+      }, 500)
     }
   }
 };
